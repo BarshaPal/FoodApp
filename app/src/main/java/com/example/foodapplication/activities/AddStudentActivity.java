a4bd8ce9353b8a031b916f63b8a85a3309af8ee2
BarshaBits <161912217+BarshaBits@users.noreply.github.com>
2024-11-15 11:25:07 +0530

    3rd cmmit

---
+++ app/src/main/java/com/example/foodapplication/activities/AddStudentActivity.java
@@ -0,0 +1,4 @@
+package com.example.foodapplication.activities;
+
+public class AddStudentActivity {
+}
---
+++ app/src/main/java/com/example/foodapplication/adapter/StudentAdapter.java
@@ -0,0 +1,4 @@
+package com.example.foodapplication.adapter;
+
+public class StudentAdapter {
+}
